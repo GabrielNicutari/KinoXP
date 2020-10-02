2d83088ac028b73e156acdd1fdb3ac68c54cd7ff
David <davidharingri@gmail.com>
2020-10-02 13:34:42 +0200

    First HTML/Controller Change

--- src/main/java/com/example/demo/controller/Home.java
+++ src/main/java/com/example/demo/controller/Home.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.repository.ActorRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Home {
 
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getHomePage() {
+        return "index";
+    }
 }
