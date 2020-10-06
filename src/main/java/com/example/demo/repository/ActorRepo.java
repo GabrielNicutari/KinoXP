3f7c37f69f0673e1f961ab820318d2da0f215423
Alexander Minchev <aleksander.minchev18@gmail.com>
2020-10-06 09:54:31 +0200

    Testing branch

---
+++ src/main/java/com/example/demo/repository/ActorRepo.java
@@ -0,0 +1,30 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Actor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+;
+@Repository
+public class ActorRepo {
+    @Autowired
+     JdbcTemplate jdbcTemplate;
+    public List<Actor> fetchAll(){
+        String query = "SELECT * FROM actor;";
+        RowMapper<Actor> rm = new BeanPropertyRowMapper<>(Actor.class);
+        List<Actor> ls = jdbcTemplate.query(query,rm);
+        System.out.println(rm);
+        return ls;
+    }
+    public Actor findByName(String name)
+    {
+        String query = "SELECT * FROM actor WHERE name LIKE `?`;";
+        RowMapper<Actor> rm = new BeanPropertyRowMapper<>(Actor.class);
+        return jdbcTemplate.queryForObject(query,rm,name);
+    }
+}
