2c9f90fba13fde48a6b8ad8c9eb3dc9afcf4b54f
Astthor <a.bragason@gmail.com>
2020-10-14 13:30:07 +0200

    Updated showtime so that the employee can select one date. Default is showing the whole week, and once a date is selected, app shows only that date.

--- src/main/java/com/example/demo/controller/ShowtimeController.java
+++ src/main/java/com/example/demo/controller/ShowtimeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -27,14 +28,15 @@ public class ShowtimeController {
     * Method as is, gets all showtimes with the movieId from todays date and for the week.
     * */
     // Add to paths - showtime/movieid/week... - showtime/movieId/dateSelected (Both with editable - adminEdit extension)
-    @RequestMapping(value = {"/showtime/{movieId}", "/showtime/{movieId}/{adminEdit}", "/showtime/{movieId}/dateSelected"})
+    /*
+    @GetMapping(value = {"/showtime/{movieId}", "/showtime/{movieId}/{adminEdit}", "/showtime/{movieId}/date/{dateSelected}"})
     public String showtimeMovieId (@PathVariable("movieId") int movieId,
                                    @PathVariable("adminEdit") Optional<String> adminEdit,
                                    @PathVariable("dateSelected") Optional<String> dateSelected,
                                    Model model){
-        ArrayList<ArrayList<Showtime>> week = showtimeService.fetchAllInWeekWithMovieId(movieId);
         if(adminEdit.isPresent()){
             String editable = adminEdit.get();
+            System.out.println("Says admin is present");
             if(editable.equals("admin1234")){
                 model.addAttribute("edit", true);
                 model.addAttribute("create", true);
@@ -45,22 +47,68 @@ public class ShowtimeController {
         }
         if(dateSelected.isPresent()){
             // Code for dateSelected.
+            System.out.println(dateSelected + "This is the date selected. Inside isPresent for date");
             String d = dateSelected.get();
+            System.out.println("After converting: " + d);
             if(showtimeService.isDate(d)){
                 LocalDate ld = LocalDate.parse(d);
-                showtimeService.fetchShowtimesWithDateAndMovieId(ld, movieId);
-                model.addAttribute("day1", showtimeService.fetchShowtimesWithDateAndMovieId(ld, movieId));
+                List<Showtime> showtimeList = showtimeService.fetchShowtimesWithDateAndMovieId(ld, movieId);
+                model.addAttribute("date", "isDate");
+                model.addAttribute("day1", showtimeList);
+                System.out.println("Gotten from showtime");
             }
         } else {
-            //model.addAttribute()
+            System.out.println("isNotDate Sent");
+            model.addAttribute("date", "isNotDate");
+            ArrayList<ArrayList<Showtime>> week = showtimeService.fetchAllInWeekWithMovieId(movieId);
+            model.addAttribute("day1", week.get(0));
+            model.addAttribute("day2", week.get(1));
+            model.addAttribute("day3", week.get(2));
+            model.addAttribute("day4", week.get(3));
+            model.addAttribute("day5", week.get(4));
+            model.addAttribute("day6", week.get(5));
+            model.addAttribute("day7", week.get(6));
         }
-        model.addAttribute("day1", week.get(0));
-        model.addAttribute("day2", week.get(1));
-        model.addAttribute("day3", week.get(2));
-        model.addAttribute("day4", week.get(3));
-        model.addAttribute("day5", week.get(4));
-        model.addAttribute("day6", week.get(5));
-        model.addAttribute("day7", week.get(6));
+        return "showtime";
+    }
+     */
+    @GetMapping(value = {"/showtime/{movieId}", "/showtime/{movieId}/{adminEdit}"})
+    public String showtimeMovieId (@PathVariable("movieId") int movieId,
+                                   @PathVariable("adminEdit") Optional<String> adminEdit,
+                                   Model model, String dateSelected){
+        if(adminEdit.isPresent()){
+            String editable = adminEdit.get();
+            System.out.println("Says admin is present");
+            if(editable.equals("admin1234")){
+                model.addAttribute("edit", true);
+                model.addAttribute("create", true);
+            } else {
+                model.addAttribute("edit", false);
+                model.addAttribute("create", false);
+            }
+        }
+        if(dateSelected != null){
+            // Code for dateSelected.
+            System.out.println(dateSelected + "This is the date selected. Inside isPresent for date");
+            if(showtimeService.isDate(dateSelected)){
+                LocalDate ld = LocalDate.parse(dateSelected);
+                List<Showtime> showtimeList = showtimeService.fetchShowtimesWithDateAndMovieId(ld, movieId);
+                model.addAttribute("date", "isDate");
+                model.addAttribute("day1", showtimeList);
+                System.out.println("Gotten from showtime");
+            }
+        } else {
+            System.out.println("isNotDate Sent");
+            model.addAttribute("date", "isNotDate");
+            ArrayList<ArrayList<Showtime>> week = showtimeService.fetchAllInWeekWithMovieId(movieId);
+            model.addAttribute("day1", week.get(0));
+            model.addAttribute("day2", week.get(1));
+            model.addAttribute("day3", week.get(2));
+            model.addAttribute("day4", week.get(3));
+            model.addAttribute("day5", week.get(4));
+            model.addAttribute("day6", week.get(5));
+            model.addAttribute("day7", week.get(6));
+        }
         return "showtime";
     }
 
